60cf8099d5bbb81851acc6d8be9a30bb00fde0cd
JB <jared.bryngelson@cerner.com>
2017-11-01 14:52:49 -0500

    more work, some testing

--- pCompare.py
+++ pCompare.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+
+import sys
+
 sFile = open("pSource.txt", "r")
 tFile = open("tSource.txt", "r")
 
@@ -5,12 +9,16 @@ sArray = sFile.readlines()
 tArray = tFile.readlines()
 
 fArray = set(tArray) - set(sArray)
+print "This is the name of the script: ", sys.argv[0]
+print "Number of arguments: ", len(sys.argv)
+print "The arguments are: " , str(sys.argv)
 
 print sorted(fArray)
 
 fFile=open("fFile.txt", "w")
-for i in sorted(fArray):
+for i in sorted(fArray, reverse=True):
     fFile.write(i)
 
 sFile.close()
 tFile.close()
+
